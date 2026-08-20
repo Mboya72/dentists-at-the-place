1ac0d4cfe3d3ec8754b7afa67435fdf3d93c8ef9
Mboya72 <elvindio72@gmail.com>
2026-08-20 14:00:05 +0300

    fix: Normalize logo file name case in Navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -62,7 +62,7 @@ export default function Navbar() {
       {/* Logo */}
       <Link href="/" className="flex items-center">
         <Image
-          src="/Logo.png"
+          src="/logo.png"
           alt="Logo"
           width={60}
           height={60}
